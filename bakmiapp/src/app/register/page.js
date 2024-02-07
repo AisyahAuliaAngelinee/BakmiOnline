88e7a5f8f78d5d475827816c84ddb2a3496d4d0d
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-08 00:11:43 +0700

    initial commit:

--- bakmiapp/src/app/register/page.js
+++ bakmiapp/src/app/register/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Register = () => {
 	return (
 		<>
@@ -8,7 +10,10 @@ const Register = () => {
 					<input type="password" placeholder="Password" />
 					<button type="submit">Register</button>
 					<div className="my-4 text-center text-gray-500">or Login with other account</div>
-					<button>Login with Google</button>
+					<button className="flex gap-4 justify-center hover:bg-blue-600 hover:text-white transition-all">
+						<Image src={"/google-icon-signin.png"} alt={"google-login"} width={24} height={24} />
+						Login with Google
+					</button>
 				</form>
 			</section>
 		</>
