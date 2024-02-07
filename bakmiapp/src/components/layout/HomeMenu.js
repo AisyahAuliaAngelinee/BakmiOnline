d9fe76bafe74e7a6b986ca336b394bf34e38febb
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-07 22:59:23 +0700

    initial commit

--- bakmiapp/src/components/layout/HomeMenu.js
+++ bakmiapp/src/components/layout/HomeMenu.js
@@ -1,10 +1,11 @@
 import Image from "next/legacy/image";
+import MenuItem from "../menu/MenuItem";
 
 const HomeMenu = () => {
 	return (
 		<>
 			<section className="">
-				<div className="absolute h-full right-0 left-0 w-full justify-start">
+				<div className="absolute right-0 left-0 w-full justify-start">
 					<div className="absolute left-0 -top-[70px] text-left -z-10">
 						<Image src={"/sallad1.png"} width={109} height={189} alt={"salad-1"} />
 					</div>
@@ -12,17 +13,20 @@ const HomeMenu = () => {
 						<Image src={"/sallad2.png"} width={107} height={195} alt={"salad-2"} />
 					</div>
 				</div>
-				<div className="text-center">
+				<div className="text-center mb-4">
 					<h3 className="uppercase text-gray-600 font-semibold leading-5">Check Our</h3>
 					<h2 className="text-primary font-bold text-4xl italic">Menu</h2>
 				</div>
 				<div className="grid grid-cols-3 gap-4">
-					<div className="bg-gray-300 p-4 rounded-lg text-center">
-						<img src="/kategori-1.png" alt="bakmi" />
-						<h4 className="font-semibold text-xl my-2">Bakmi</h4>
-						<p className="text-gray-500 text-sm">Lorem ipsum bakmi sit amet</p>
-						<button className="bg-primary text-white rounded-full px-6 py-2 hover:bg-red-500">Add to cart Rp.15.000,00</button>
-					</div>
+					<MenuItem />
+					<MenuItem />
+					<MenuItem />
+					<MenuItem />
+					<MenuItem />
+					<MenuItem />
+					<MenuItem />
+					<MenuItem />
+					<MenuItem />
 				</div>
 			</section>
 		</>
---
+++ bakmiapp/src/components/menu/MenuItem.js
@@ -0,0 +1,14 @@
+const MenuItem = () => {
+	return (
+		<>
+			<div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white">
+				<img src="/kategori-1.png" alt="bakmi" />
+				<h4 className="font-semibold text-xl my-3">Bakmi</h4>
+				<p className="text-gray-500 text-sm">Lorem ipsum bakmi sit amet</p>
+				<button className="mt-4 bg-primary text-white rounded-full px-8 py-2 hover:bg-red-500">Add to cart Rp.15.000,00</button>
+			</div>
+		</>
+	);
+};
+
+export default MenuItem;
