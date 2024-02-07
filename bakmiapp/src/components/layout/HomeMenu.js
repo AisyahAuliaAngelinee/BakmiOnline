642965cb75c2a580114d9aee3fa686108f9d8a64
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-07 22:13:34 +0700

    initial commit

--- bakmiapp/src/components/layout/HomeMenu.js
+++ bakmiapp/src/components/layout/HomeMenu.js
@@ -4,11 +4,13 @@ const HomeMenu = () => {
 	return (
 		<>
 			<section className="">
-				<div className="h-48 relative">
-					<Image src={"/kategori-1.png"} layout={"fill"} objectFit={"contain"} alt={"kategori-1"} />
-				</div>
-				<div className="h-48 relative">
-					<Image src={"/kategori-1.png"} layout={"fill"} objectFit={"contain"} alt={"kategori-1"} />
+				<div className="absolute right-0 left-0">
+					<div className="h-48 w-48 left-0 absolute">
+						<Image src={"/kategori-1.png"} layout={"fill"} objectFit={"contain"} alt={"kategori-1"} />
+					</div>
+					<div className="h-48 w-48 right-0 absolute">
+						<Image src={"/kategori-2.png"} layout={"fill"} objectFit={"contain"} alt={"kategori-2"} />
+					</div>
 				</div>
 				<div className="text-center">
 					<h3 className="uppercase text-gray-600 font-semibold leading-5">Check Our</h3>
