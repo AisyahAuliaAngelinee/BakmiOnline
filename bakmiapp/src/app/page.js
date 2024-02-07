bee03cd8bcb02080de3645456184170848ddaa62
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-07 20:48:51 +0700

    initial commit

--- bakmiapp/src/app/page.js
+++ bakmiapp/src/app/page.js
@@ -1,22 +1,9 @@
-import Link from "next/link";
+import Header from "@/components/layout/Header";
 
 export default function Home() {
 	return (
 		<>
-			<header className="flex items-center justify-between">
-				<Link className="text-primary font-semibold text-2xl" href={""}>
-					BAKMI ONLINE
-				</Link>
-				<nav className="flex items-center gap-4 text-gray-500 font-semibold">
-					<Link href={""}>Home</Link>
-					<Link href={""}>Menu</Link>
-					<Link href={""}>About</Link>
-					<Link href={""}>Contact</Link>
-					<Link href={""} className="bg-primary rounded-full text-white px-6 py-2">
-						Login
-					</Link>
-				</nav>
-			</header>
+			<Header />
 		</>
 	);
 }
