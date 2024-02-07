a578bff951a721072baac43159871d6be4b0c43e
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-07 21:52:00 +0700

    initial commit

---
+++ bakmiapp/src/components/layout/HomeMenu.js
@@ -0,0 +1,14 @@
+const HomeMenu = () => {
+	return (
+		<>
+			<section className="">
+				<div className="text-center">
+					<h3 className="uppercase text-gray-600 font-semibold leading-5">Check Our</h3>
+					<h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default HomeMenu;
