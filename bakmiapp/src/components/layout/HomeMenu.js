a7850c502964c08f16163ffd49dbc00ccc0e7bbf
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-07 21:58:29 +0700

    initial commit:

--- bakmiapp/src/components/layout/HomeMenu.js
+++ bakmiapp/src/components/layout/HomeMenu.js
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 const HomeMenu = () => {
 	return (
 		<>
 			<section className="">
+				<div className="h-48 relative">
+					<Image src={"/kategori-1.png"} layout={"fill"} objectFit={"contain"} alt={"kategori-1"} />
+				</div>
+				<div className="h-48 relative">
+					<Image src={"/kategori-1.png"} layout={"fill"} objectFit={"contain"} alt={"kategori-1"} />
+				</div>
 				<div className="text-center">
 					<h3 className="uppercase text-gray-600 font-semibold leading-5">Check Our</h3>
 					<h2 className="text-primary font-bold text-4xl italic">Menu</h2>
