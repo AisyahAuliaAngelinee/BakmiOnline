54adab39f09b7d7cd4a3ee120636bc318a7ba462
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-07 21:17:41 +0700

    initial commit

--- bakmiapp/src/components/layout/Hero.js
+++ bakmiapp/src/components/layout/Hero.js
@@ -5,14 +5,16 @@ const Hero = () => {
 	return (
 		<>
 			<section className="grid grid-cols-2">
-				<div>
+				<div className="py-8">
 					<h1 className="text-4xl font-semibold">Everyday will better with Bakmi</h1>
 					<p className="my-4 text-gray-500">100% Halal no pork, Certified Bakmi with Scholarship Approved. Want to live healthy? eat Bakmi all the time</p>
-					<div className="flex gap-4">
-						<button className="bg-primary text-white px-8 py-2 rounded-full">
+					<div className="flex gap-4 text-sm">
+						<button className="flex gap-2 bg-primary text-white uppercase font-semibold items-center px-4 py-2 rounded-full">
 							Order Now <Right />
 						</button>
-						<button>Learn more</button>
+						<button className="flex gap-2 py-2 text-gray-500 font-semibold">
+							Learn more <Right />
+						</button>
 					</div>
 				</div>
 				<div className="relative">
