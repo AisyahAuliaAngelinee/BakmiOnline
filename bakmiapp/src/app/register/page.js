911fcd9376ab8b1bb8de81d53b140be170649c57
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-08 00:07:35 +0700

    initial commit

--- bakmiapp/src/app/register/page.js
+++ bakmiapp/src/app/register/page.js
@@ -2,11 +2,13 @@ const Register = () => {
 	return (
 		<>
 			<section className="mt-8">
-				<h1 className="text-center text-primary text-4xl font-semibold">Register</h1>
-				<form>
+				<h1 className="text-center text-primary text-4xl font-semibold mb-4">Register</h1>
+				<form className="block max-w-xs mx-auto">
 					<input type="text" placeholder="Email" />
 					<input type="password" placeholder="Password" />
 					<button type="submit">Register</button>
+					<div className="my-4 text-center text-gray-500">or Login with other account</div>
+					<button>Login with Google</button>
 				</form>
 			</section>
 		</>
