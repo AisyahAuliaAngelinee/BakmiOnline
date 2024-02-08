91ea41620a330df978b17d96f73e76664084d24d
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-08 19:15:11 +0700

    initial commit

--- bakmiapp/src/components/layout/HomeMenu.js
+++ bakmiapp/src/components/layout/HomeMenu.js
@@ -7,10 +7,10 @@ const HomeMenu = () => {
 		<>
 			<section className="">
 				<div className="absolute right-0 left-0 w-full justify-start">
-					<div className="absolute left-0 -top-[70px] text-left -z-10">
+					<div className="absolute left-[10rem] -top-[70px] text-left -z-10 scale-[5]">
 						<Image src={"/sallad1.png"} width={109} height={189} alt={"salad-1"} />
 					</div>
-					<div className="absolute right-0 -top-[100px] -z-10">
+					<div className="absolute right-[13rem] -top-[100px] -z-10 scale-[5]">
 						<Image src={"/sallad2.png"} width={107} height={195} alt={"salad-2"} />
 					</div>
 				</div>
