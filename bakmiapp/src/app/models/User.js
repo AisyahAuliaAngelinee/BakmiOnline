c4727b75b5cf38c2b362e3aa0b0940931f88651f
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-02-08 09:05:45 +0700

    initial commit

--- bakmiapp/src/app/models/User.js
+++ bakmiapp/src/app/models/User.js
@@ -1,13 +1,21 @@
-const { Schema } = require("mongoose");
+const { Schema, models, model } = require("mongoose");
 
-const UserSchema = new Schema({
-	email: { type: String, require: true, unique: true },
-	password: {
-		type: String,
-		require: true,
-		validate: (pass) => {
-			if (!pass?.length || pass?.length < 5) {
-			}
+const UserSchema = new Schema(
+	{
+		email: { type: String, require: true, unique: true },
+		password: {
+			type: String,
+			require: true,
+			validate: (pass) => {
+				if (!pass?.length || pass?.length < 5) {
+					new Error("Password must be at least 5 characters");
+				}
+			},
 		},
 	},
-});
+	{
+		timestamps: true,
+	}
+);
+
+export const User = models?.User || model("User", UserSchema);
--- bakmiapp/src/components/menu/MenuItem.js
+++ bakmiapp/src/components/menu/MenuItem.js
@@ -7,7 +7,7 @@ const MenuItem = () => {
 				</div>
 				<h4 className="font-semibold text-xl my-3">Bakmi</h4>
 				<p className="text-gray-500 text-sm">Lorem ipsum bakmi sit amet</p>
-				<button className="mt-4 bg-primary text-white rounded-full px-8 py-2 hover:bg-red-500 transition-all">Add to cart Rp.15.000,00</button>
+				<button className="mt-4 bg-primary text-white rounded-full px-6 py-2 hover:bg-red-500 transition-all">Add to cart Rp.15.000,00</button>
 			</div>
 		</>
 	);
